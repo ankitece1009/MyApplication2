61d7a033f43f6a8e69c67d17713eb3d4b75f4960
ankitece1009 <ankitece1009@gmail.com>
2017-11-17 00:03:59 +0530

    onSaveInstance added

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.admin.myapplication;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
@@ -30,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     };*/
     Map<String,String> map ;
     List<String> mylist;
+    MediaPlayer mp ;
+    private int points;
     void ReadfromBothFiles() {
         Scanner sc1 = new Scanner(getResources().openRawResource(R.raw.hindiwords));
         ReadFromFile(sc1);
@@ -107,6 +111,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        points = 0 ;
+        mp = MediaPlayer.create(this,R.raw.despacito);
+        mp.start();
         map = new HashMap<>();
         mylist = new ArrayList<>();
         /*for(int i=0;i<str.length;i+=2)
@@ -144,13 +151,46 @@ public class MainActivity extends AppCompatActivity {
                 TextView myText = (TextView)findViewById(R.id.the_word);
                 String  key = myText.getText().toString();
                 String correct = map.get(key);
-                if(chosen.equals(correct))
-                    Toast.makeText(getApplicationContext(),"AweSome!",Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(getApplicationContext(),":-{,In correct.",Toast.LENGTH_SHORT).show();
+                if(chosen.equals(correct)) {
+                    points++;
+                    Toast.makeText(getApplicationContext(), "AweSome! Points="+points, Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    points--;
+                    Toast.makeText(getApplicationContext(), ":-{,In correct. Points="+points, Toast.LENGTH_SHORT).show();
+                }
                 wordGame();
             }
         });
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mp.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mp.start();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            outState.putInt("points",points);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+            super.onRestoreInstanceState(savedInstanceState);
+            points = (int)savedInstanceState.get("points");
+    }
+
+    public void AddNewWordToFile(View view) {
+        Intent intent = new Intent(this, AddWordActivity.class);
+        startActivity(intent);
     }
 }
